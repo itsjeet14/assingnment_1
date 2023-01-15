857483ad17e5c5bd388e53dd2d342e9456ff9d3d
itsjeet14 <121816934+itsjeet14@users.noreply.github.com>
2023-01-15 22:36:41 +0530

    Question_2_Assignment_1

---
+++ _1_assignment_ques_2.py
@@ -0,0 +1,7 @@
+# Write a Python program that accepts a word from the user and reverse it.
+# Sample Test Case
+# Input : Edyoda
+# output: adoydE
+
+var = input("Enter a word: ")
+print("Reverse of above word: ",var[::-1])
